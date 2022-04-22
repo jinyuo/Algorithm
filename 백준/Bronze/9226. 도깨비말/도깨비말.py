b64f0741abc1bb0de7425a4989e8e5d4a6b868f9
jinyuo <aleydis95@gmail.com>
2022-04-22 14:40:43 +0900

    [Bronze II] Title: 도깨비말, Time: 64 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/9226. 도깨비말/도깨비말.py
+++ 백준/Bronze/9226. 도깨비말/도깨비말.py
@@ -1,12 +1,10 @@
-import sys
-vowel = "aeiou"
 while True:
-    word = sys.stdin.readline()[:-1]
-    if word == "#": exit()
-    if word[0] in vowel or len([x for x in vowel if x in word]) == 0:
-        print(word + "ay")
-    else:
-        for i in range(len(word)):
-            if word[i] in vowel:
-                print(word[i:] + word[:i] + "ay")
-                break
+    s = input()
+    if s == '#' : break
+
+    k = 0
+    for i in range(len(s)):
+        if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u':
+            k = i
+            break
+    print(s[k:] + s[:k] + 'ay')
