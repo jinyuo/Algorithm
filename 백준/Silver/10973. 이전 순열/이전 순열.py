bf1cd66ee784fe77ad5f30dbb83cbf3f783f3fc3
jinyuo <aleydis95@gmail.com>
2022-04-28 18:54:15 +0900

    [Silver III] Title: 이전 순열, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,21 @@
+import sys
+n = int(sys.stdin.readline())
+t = list(map(int, sys.stdin.readline().split()))
+i = n - 1
+while i > 0 and t[i - 1] < t[i]:
+    i -= 1
+if i <= 0:
+    print(-1)
+    exit()
+
+j = n - 1
+while t[j] > t[i - 1]:
+    j -= 1
+t[i - 1], t[j] = t[j], t[i - 1]
+
+j = n - 1
+while i < j:
+    t[i], t[j] = t[j], t[i]
+    i += 1
+    j -= 1
+print(*t)
