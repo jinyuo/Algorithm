79ef48394ce1440a74d189f81bd5a2bd689b66d2
jinyuo <aleydis95@gmail.com>
2022-05-03 20:07:19 +0900

    [Silver IV] Title: 숫자놀이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1755. 숫자놀이/숫자놀이.py
+++ 백준/Silver/1755. 숫자놀이/숫자놀이.py
@@ -1,10 +1,9 @@
 import sys
 m, n = map(int, sys.stdin.readline().split())
-intstr = {"0": "zero", "1": 'one', "2": 'two', "3": 'three', "4": 'four', "5": "five", "6": "six", "7": "seven",
-          "8": "eight", "9": "nine"}
+intstr = ["zero", 'one', 'two', 'three', 'four', "five", "six", "seven", "eight", "nine"]
 d = dict()
 for i in range(m, n + 1):
-    d[i] = " ".join([intstr[stri] for stri in str(i)])
+    d[i] = " ".join([intstr[int(stri)] for stri in str(i)])
 sort = sorted(d, key=lambda x: d[x])
 for i in range(1, len(sort) + 1):
     print(sort[i - 1], end=" ")
