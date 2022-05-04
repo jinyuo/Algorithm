72805260ba83ceb0ab19151f3f3859018703fbf0
jinyuo <aleydis95@gmail.com>
2022-05-04 21:27:40 +0900

    [Bronze I] Title: 분수찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1193. 분수찾기/분수찾기.py
+++ 백준/Bronze/1193. 분수찾기/분수찾기.py
@@ -3,17 +3,13 @@ i = 1
 x = int(sys.stdin.readline())
 while x > i * (i + 1) // 2:
     i += 1
-if i % 2 == 1:
-    c = i
-    p = 1
 
-    for _ in range(i * (i - 1) // 2, x - 1):
-        c -= 1
-        p += 1
-else:
-    c = 1
-    p = i
-    for _ in range(i * (i - 1) // 2, x - 1):
-        c += 1
-        p -= 1
+c = i
+p = 1
+for _ in range(i * (i - 1) // 2, x - 1):
+    c -= 1
+    p += 1
+
+if i % 2 == 0:
+    c, p = p, c
 print(f"{c}/{p}")
