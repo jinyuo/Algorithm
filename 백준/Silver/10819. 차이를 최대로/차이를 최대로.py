bbbfa163a5877d1832f259adfb59bd52a5e6a586
jinyuo <aleydis95@gmail.com>
2022-05-08 15:05:42 +0900

    [Silver II] Title: 차이를 최대로, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -5,20 +5,14 @@ sort_nums = []
 c = True
 while nums:
     if len(nums) == 1:
-        if abs(sort_nums[0] - nums[0]) > abs(sort_nums[-1] - nums[0]):
-            sort_nums.insert(0, nums[0])
-        else:
-            sort_nums.append(nums[0])
+        sort_nums.insert(0 if abs(sort_nums[0] - nums[0]) > abs(sort_nums[-1] - nums[0]) else n - 1, nums[0])
         break
-    min_num = nums.pop(0)
-    max_num = nums.pop()
-
     if c:
-        sort_nums.insert(0, max_num)
-        sort_nums.append(min_num)
+        sort_nums.insert(0, nums.pop())
+        sort_nums.append(nums.pop(0))
         c = False
     else:
-        sort_nums.insert(0, min_num)
-        sort_nums.append(max_num)
+        sort_nums.insert(0, nums.pop(0))
+        sort_nums.append(nums.pop())
         c = True
 print(sum([abs(sort_nums[i] - sort_nums[i + 1]) for i in range(n - 1)]))
