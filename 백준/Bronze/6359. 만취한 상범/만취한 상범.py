b4a490e7d416aa209c99f0690f77ab577c012764
jinyuo <aleydis95@gmail.com>
2022-05-11 17:33:56 +0900

    [Bronze II] Title: 만취한 상범, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/6359. 만취한 상범/만취한 상범.py
+++ 백준/Bronze/6359. 만취한 상범/만취한 상범.py
@@ -3,8 +3,4 @@ t = int(sys.stdin.readline())
 
 for _ in range(t):
     n = int(sys.stdin.readline())
-    c = [True for _ in range(n + 1)]
-    for i in range(2, n+1):
-        for j in range(0, n + 1, i):
-            c[j] = not c[j]
-    print(c[1:].count(True))
+    print(int(n ** 0.5))
