e3212b5d6d26b4bfc4677be56a8e833489d7fcde
jinyuo <aleydis95@gmail.com>
2022-05-15 13:29:39 +0900

    [Bronze II] Title: 거리의 합, Time: 252 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/2399. 거리의 합/거리의 합.py
+++ 백준/Bronze/2399. 거리의 합/거리의 합.py
@@ -2,7 +2,7 @@ n = int(__import__('sys').stdin.readline())
 nums = list(map(int, __import__('sys').stdin.readline().split()))
 nums.sort()
 s = 0
-for i in range(n):
+for i in range(n - 1):
     for j in range(i + 1, n):
         s += 2 * (nums[j] - nums[i])
 print(s)
