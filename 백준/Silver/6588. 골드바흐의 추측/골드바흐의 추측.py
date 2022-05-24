4123e56f22caa35867f5f7a4725cb52c6325b276
jinyuo <aleydis95@gmail.com>
2022-05-24 22:45:08 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 364 ms, Memory: 39196 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,30 @@
+import sys
+
+def prime(n):
+    nums = [True for _ in range(n + 1)]
+    nums[0] = False
+    nums[1] = False
+    for i in range(2, n + 1):
+        if nums[i]:
+            for j in range(i * i, n + 1, i):
+                nums[j] = False
+    return nums
+
+
+n_list = []
+while True:
+    n = int(sys.stdin.readline())
+    if n == 0:
+        break
+    n_list.append(n)
+
+prime_nums = prime(max(n_list))
+for n in n_list:
+    f = False
+    for i in range(1, len(prime_nums), 2):
+        if prime_nums[i] and prime_nums[n - i]:
+            print(f"{n} = {i} + {n - i}")
+            f = True
+            break
+    if not f:
+        print("Goldbach's conjecture is wrong.")
