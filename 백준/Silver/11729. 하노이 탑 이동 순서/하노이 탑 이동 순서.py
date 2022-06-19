f142a436c14ff27d70b74e9bbe3e6a278e69a46b
jinyuo <aleydis95@gmail.com>
2022-06-19 13:49:07 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 900 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,16 +1,13 @@
 import sys
 def hanoi(n, from_p, to_p, aux_p):
     if n == 1:
-        li.append((from_p, to_p))
+        print(from_p, to_p)
         return
     hanoi(n - 1, from_p, aux_p, to_p)
-    li.append((from_p, to_p))
+    print(from_p, to_p)
     hanoi(n - 1, aux_p, to_p, from_p)
 
 
 n = int(sys.stdin.readline())
-li = []
+print(2 ** n - 1)
 hanoi(n, 1, 3, 2)
-print(len(li))
-for a, b in li:
-    print(a, b)
