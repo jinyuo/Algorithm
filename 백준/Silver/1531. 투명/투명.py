c2f4c65076c8913a462295167d2a28796e4957c3
jinyuo <aleydis95@gmail.com>
2022-07-10 15:00:42 +0900

    [Silver V] Title: 투명, Time: 132 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1531. 투명/투명.py
+++ 백준/Silver/1531. 투명/투명.py
@@ -6,4 +6,10 @@ for _ in range(n):
     for x in range(x1 - 1, x2):
         for y in range(y1 - 1, y2):
             mosaic[x][y] += 1
-print(len([mosaic[i][j] for i in range(100) for j in range(100) if mosaic[i][j] > m]))
+
+cnt = 0
+for i in range(100):
+    for j in range(100):
+        if mosaic[i][j] > m:
+            cnt += 1
+print(cnt)
