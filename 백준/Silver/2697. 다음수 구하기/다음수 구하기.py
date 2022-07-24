57ffa97e9e26751f1ce3228686f3d013cc94ff9b
jinyuo <aleydis95@gmail.com>
2022-07-24 19:33:20 +0900

    [Silver II] Title: 다음수 구하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2697. 다음수 구하기/다음수 구하기.py
@@ -0,0 +1,21 @@
+import sys
+t = int(sys.stdin.readline())
+for _ in range(t):
+    a = list(str(sys.stdin.readline()[:-1]))
+    a = [int(i) for i in a]
+
+    i = len(a) - 1
+    while i > 0 and a[i - 1] >= a[i]:
+        i -= 1
+    if i <= 0:
+        print("BIGGEST")
+        continue
+
+    j = len(a) - 1
+    while a[j] <= a[i - 1]:
+        j -= 1
+
+    a[i - 1], a[j] = a[j], a[i - 1]
+
+    a = a[:i] + a[i:][::-1]
+    print("".join(str(i) for i in a))
