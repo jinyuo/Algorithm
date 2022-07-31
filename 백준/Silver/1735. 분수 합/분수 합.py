140cfdb1fdd2757e04be099113ed2d79194a1cef
jinyuo <aleydis95@gmail.com>
2022-07-31 15:00:03 +0900

    [Silver III] Title: 분수 합, Time: 72 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.py
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -1,6 +1,6 @@
-import sys, math
-a1, b1 = map(int, sys.stdin.readline().split())
-a2, b2 = map(int, sys.stdin.readline().split())
+import math
+a1, b1 = map(int, input().split())
+a2, b2 = map(int, input().split())
 b = math.lcm(b1, b2)
 a = a1 * (b // b1) + a2 * (b // b2)
 r = math.gcd(a, b)
