be48a2565642dec0bfa82cec5b6d267259e7546a
jinyuo <aleydis95@gmail.com>
2023-07-08 00:54:25 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,14 +1,10 @@
 def solution(cards1, cards2, goal):
     answer = 'Yes'
     for g in goal:
-        # try:
         if cards1 and g == cards1[0]:
             del cards1[0]
         elif cards2 and g == cards2[0]:
             del cards2[0]
         else:
-            answer = 'No'
-        # except:
-        #     answer = 'No'
-    
+            answer = 'No'    
     return answer
