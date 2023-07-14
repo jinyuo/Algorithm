326ca2c28f7b02bec6761c68686588ea3c79f848
jinyuo <aleydis95@gmail.com>
2023-07-14 20:34:00 +0900

    [unrated] Title: 덧칠하기, Time: 1.16 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,11 @@
+import math 
+
+def solution(n, m, section):
+    answer = 0
+    now_section = 0
+    for s in section:
+        if now_section < s:
+            now_section = s + m - 1
+            answer += 1          
+            
+    return answer
