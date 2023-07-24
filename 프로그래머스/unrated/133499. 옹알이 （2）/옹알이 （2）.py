18a3ebeacc06cf3085547303e8e93b0c4e1a308b
jinyuo <aleydis95@gmail.com>
2023-07-24 21:54:43 +0900

    [unrated] Title: 옹알이 (2), Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -2,23 +2,10 @@ def solution(babbling):
     defi = ["aya", "ye", "woo", "ma"]
     answer = 0
     for b in babbling:
-        pre = ''
-        while b != '':
-            if pre != defi[0] and b.startswith(defi[0]):
-                pre = defi[0]
-                b = b[len(defi[0]):]
-            elif pre != defi[1] and b.startswith(defi[1]):
-                pre = defi[1]
-                b = b[len(defi[1]):]
-            elif pre != defi[2] and b.startswith(defi[2]):
-                pre = defi[2]
-                b = b[len(defi[2]):]
-            elif pre != defi[3] and b.startswith(defi[3]):
-                pre = defi[3]
-                b = b[len(defi[3]):]
-            else:
-                break
-
+        for d in defi:
+            if d * 2 not in b:
+                b = b.replace(d, ' ')
+        b = b.replace(' ', '')
         if not b:
             answer += 1
     
