28dc2da8fdf3ba1e6a240bac76cb27e9eb723907
jinyuo <aleydis95@gmail.com>
2023-07-30 18:34:47 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,39 +1,22 @@
 import re
 
 def solution(new_id):
-    allow_str = '[a-z]|[0-9]|-|_|\.'
+    allow_str = '[^a-z0-9\-_\.]'
     max_len = 16
     min_len = 3
     
-    # step - 1
     new_id = new_id.lower()
+    new_id = re.sub(allow_str, '', new_id)
+    new_id = re.sub('\.+', '.', new_id)
+    new_id = re.sub('^\.|\.$', '', new_id)
     
-    # step - 2
-    new_id = ''.join(re.findall(allow_str, new_id))
-
-    # step - 3
-    while '..' in new_id:
-        new_id = new_id.replace('..', '.')
-    
-    # step - 4
-    while re.search('^\.', new_id):
-        new_id = new_id[1:]
-    while re.search('\.$', new_id):
-        new_id = new_id[:-1]    
-    
-    # step - 5
     if not new_id:
         new_id = 'a'
-    
-    # step - 6
-    if len(new_id) >= max_len:
+    elif len(new_id) >= max_len:
         new_id = new_id[:max_len - 1]
-    while re.search('\.$', new_id):
-        new_id = new_id[:-1]    
-        
-    # step - 7
+    new_id = re.sub('\.$', '', new_id)
+
     if len(new_id) < min_len:
-        new_id += new_id[-1] * min_len
-        new_id = new_id[:min_len]
+        new_id = (new_id + new_id[-1] * min_len)[:min_len]
     
     return new_id
