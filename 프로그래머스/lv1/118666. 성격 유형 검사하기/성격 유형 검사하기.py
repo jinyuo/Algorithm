5819ca6d69ec41e4e04b6dd7b8671d3bc6fb9ce9
jinyuo <aleydis95@gmail.com>
2023-08-01 23:07:12 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.38 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,16 +1,13 @@
 def solution(survey, choices):
-    mbti = dict()
-    metrics = ['RT', 'CF', 'JM', 'AN']
+    mbti = {'RT': 0, 'CF': 0, 'JM': 0, 'AN': 0}
     choice_standard = 4
     for s, c in zip(survey, choices):
         if s in mbti.keys():
             mbti[s] += (choice_standard - c)
+        elif s[::-1] in mbti.keys():
+            mbti[s[::-1]] -= (choice_standard - c)
         else:
             mbti[s] = choice_standard - c
-    
-    tmp_mbti = dict()
-    for m in metrics:
-        tmp_mbti[m] = mbti.get(m, 0) - mbti.get(m[::-1], 0)
-    
-    answer = ''.join(m[0] if tmp_mbti[m] >= 0 else m[-1] for m in tmp_mbti.keys())
+
+    answer = ''.join(m[0] if mbti[m] >= 0 else m[-1] for m in mbti.keys())
     return answer
