7a08c0ef1177de6b0c92a653c04d2dbb5b71d2f6
jinyuo <aleydis95@gmail.com>
2023-08-12 22:54:23 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 2.75 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from dateutil.relativedelta import relativedelta 
+
 def solution(today, terms, privacies):
     today = datetime.strptime(today, '%Y.%m.%d')
     map_term = dict()
@@ -8,12 +10,8 @@ def solution(today, terms, privacies):
     
     answer = []
     for i, p in enumerate(privacies):
-        start_date, ty = p.split()
-        start_date = datetime.strptime(start_date, '%Y.%m.%d')
-        add_month = start_date.month + map_term[ty] - 1
-        year = start_date.year + int(add_month / 12)
-        month = add_month % 12 + 1
-        end_date = datetime(year, month, start_date.day)
+        start_date, tp = p.split()       
+        end_date = datetime.strptime(start_date, '%Y.%m.%d') + relativedelta(months=map_term[tp])
         if today >= end_date:
             answer.append(i + 1)
     return answer
