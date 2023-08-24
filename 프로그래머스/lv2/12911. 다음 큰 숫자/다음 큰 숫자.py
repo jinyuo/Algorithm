f0855570030f4ea263fc8d2f038e4d8f8abbdf45
jinyuo <aleydis95@gmail.com>
2023-08-24 12:21:19 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,9 +1,8 @@
 def solution(n):
     bin_n = '0' + bin(n)[2:]
-    idx_one = bin_n.rfind('1')
-    idx_zero = bin_n.rfind('0', 0, idx_one)
-    cnt_zero = len(bin_n) - idx_one - 1
-    cnt_one = idx_one - idx_zero - 1
-    answer = bin_n[:idx_zero] + bin_n[idx_zero:idx_zero + 2][::-1] + '0' * cnt_zero + '1' * cnt_one
+    idx_swap = bin_n.rfind('01')
+    cnt_zero = bin_n[idx_swap + 2:].count('0')
+    cnt_one = bin_n[idx_swap + 2:].count('1')
+    answer = bin_n[:idx_swap] + bin_n[idx_swap:idx_swap + 2][::-1] + '0' * cnt_zero + '1' * cnt_one
     
     return int(answer, base=2)
