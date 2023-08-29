5713eaac97650b8f308629bcfefe7f7c3578adfa
jinyuo <aleydis95@gmail.com>
2023-08-29 21:34:40 +0900

    [level 2] Title: 구명보트, Time: 9.05 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -6,10 +6,8 @@ def solution(people, limit):
     end = len(people) - 1
     while start <= end:
         if (people[start] + people[end]) <= limit:
-            start += 1
             end -= 1
-        else:
-            start += 1
+        start += 1
         answer += 1
 
-    return answer
+    return answer 
