f983ea254e1c4b144eb41ab8df3d52ceef79f41b
jinyuo <aleydis95@gmail.com>
2023-09-03 16:47:31 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,9 +1,2 @@
-import math
-
 def solution(n,a,b):
-    answer = 0
-    while a != b:
-        a = math.ceil(a / 2)
-        b = math.ceil(b / 2)
-        answer += 1
-    return answer
+    return ((a-1)^(b-1)).bit_length()
