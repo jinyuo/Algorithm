b813be49983c477d982a2611b3bffd10507fd2df
jinyuo <aleydis95@gmail.com>
2023-09-22 18:51:04 +0900

    [unrated] Title: 귤 고르기, Time: 29.99 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -2,10 +2,9 @@ from collections import Counter
 
 def solution(k, tangerine):
     t_group = Counter(tangerine)
-    t_group = sorted(t_group.items(), key=lambda x: x[1], reverse=True)
     answer = 0
     cnt = 0
-    for g, c in t_group:
+    for c in sorted(t_group.values(), reverse=True):
         if cnt < k:
             cnt += c
             answer += 1
