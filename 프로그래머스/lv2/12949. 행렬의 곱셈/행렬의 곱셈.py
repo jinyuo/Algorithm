cfdd8b688e0356c696d213253599b2a2b0f44e97
jinyuo <aleydis95@gmail.com>
2023-10-02 20:30:18 +0900

    [level 2] Title: 행렬의 곱셈, Time: 4.77 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,12 +1,2 @@
-def solution(arr1, arr2):
-    rows = len(arr1)
-    cols = len(arr2[0])
-    answer = [[-1 for i in range(cols)] for j in range(rows)]
-    
-    for i in range(rows):
-        for j in range(cols):
-            arr1_a = arr1[i]
-            arr2_b = [a[j] for a in arr2]
-            answer[i][j] = sum([arr1_a[t] * arr2_b[t] for t in range(len(arr1_a))])    
-    
-    return answer
+def solution(A, B):
+    return [[sum(a*b for a, b in zip(A_row,B_col)) for B_col in zip(*B)] for A_row in A]
