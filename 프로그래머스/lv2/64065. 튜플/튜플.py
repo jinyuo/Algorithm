1c3a0441d1ef64f50fb65b934c3e2a5dff738abc
jinyuo <aleydis95@gmail.com>
2023-10-06 17:09:09 +0900

    [level 2] Title: 튜플, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,4 +1,6 @@
+from collections import Counter
+
 def solution(s):
     s = list(map(int, s.replace('{', '').replace('}', '').split(',')))
-    answer = sorted(list(set(s)), key=lambda x: s.count(x), reverse=True)
-    return answer
+    tple = [c[0] for c in Counter(s).most_common()]
+    return tple
