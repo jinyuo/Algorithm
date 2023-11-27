dc10704a71b6b40578260b200305bbcc2f13efc9
jinyuo <aleydis95@gmail.com>
2023-11-27 21:23:50 +0900

    DELETE unrated

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++
@@ -1,3 +0,0 @@
-def solution(a, b, n):
-    answer = (n - b) // (a - b) * b
-    return answer
--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++
@@ -1,5 +0,0 @@
-import heapq
-
-def solution(k, score):
-    answer = [min(heapq.nlargest(min(i, k), score[:i])) for i in range(1, len(score) + 1)]
-    return answer
