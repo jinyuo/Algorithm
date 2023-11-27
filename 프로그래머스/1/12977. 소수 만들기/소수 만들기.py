5eece4c65f99db4d16c375e0780460edf3c92105
jinyuo <aleydis95@gmail.com>
2023-11-27 20:50:40 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+
+def prime_list(n):
+    sieve = [True] * n
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i]:           
+            for j in range(i+i, n, i): 
+                sieve[j] = False
+    return [i for i in range(2, n) if sieve[i]]
+
+
+def solution(nums):
+    answer = -1
+    list_sum = [sum(combi) for combi in combinations(nums, 3)]
+    list_prime = prime_list(max(list_sum) + 1)    
+    return len([s for s in list_sum if s in list_prime])
