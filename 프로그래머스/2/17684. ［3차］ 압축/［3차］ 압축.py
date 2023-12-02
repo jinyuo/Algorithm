a5741e190718f16e7c7b5e717924c7ce8e5208e3
jinyuo <aleydis95@gmail.com>
2023-12-02 19:58:03 +0900

    [level 2] Title: [3차] 압축, Time: 0.24 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,13 +1,14 @@
 def solution(msg):
+    w = ''
     zip_dict = {i: ord(i) - 64 for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'}
     answer = []
-    while msg:
-        list_key = list(zip_dict.keys())[::-1]
-        for k in list_key:
-            if msg.startswith(k):
-                answer.append(zip_dict[k])
-                msg = msg.replace(k, '', 1)
-                if msg:
-                    zip_dict[k + msg[0]] = len(list_key) + 1
-                break
+    for c in msg:
+        list_keys = zip_dict.keys()
+        if w + c in list_keys:
+            w = w + c
+        else:
+            zip_dict[w + c] = len(list_keys) + 1
+            answer.append(zip_dict[w])
+            w = c
+    answer.append(zip_dict[w])
     return answer
