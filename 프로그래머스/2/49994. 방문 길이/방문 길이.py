f9299d7ab429c2accab1f2483973719714df884b
jinyuo <aleydis95@gmail.com>
2023-12-12 23:19:54 +0900

    [level 2] Title: 방문 길이, Time: 0.23 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,16 +1,11 @@
 def solution(dirs):
     src = (0, 0)
-    
+    mv_units = {'U': (0, 1), 'D': (0, -1), 'R': (1, 0), 'L': (-1, 0)}
+
     ways = set()
     for d in dirs:
-        if d == 'U':
-            dest = (src[0], src[1] + 1)
-        if d == 'D':
-            dest = (src[0], src[1] - 1)
-        if d == 'L':
-            dest = (src[0] - 1, src[1])
-        if d == 'R':
-            dest = (src[0] + 1, src[1])
+        m = mv_units[d]
+        dest = (src[0] + m[0], src[1] + m[1])
         
         if -5 <= dest[0] <= 5 and -5 <= dest[1] <= 5:
             if (dest, src) not in ways:
