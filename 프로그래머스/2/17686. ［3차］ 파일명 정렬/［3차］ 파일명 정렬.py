2ff99305e22b0aaad8de78e7f227bfac8646aa78
jinyuo <aleydis95@gmail.com>
2024-01-01 19:50:08 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 4.13 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -4,7 +4,7 @@ def solution(files):
     answer = []
 
     for f in files:
-        split = re.match('(?P<head>\D+)(?P<number>\d+)(?P<tail>.*)', f).groupdict()
+        split = re.match('(?P<head>\D+)(?P<number>\d{1,5})(?P<tail>.*)', f).groupdict()
         split['file'] = f
         split['head'] = split['head'].lower()
         split['number'] = int(split['number'])
