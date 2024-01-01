f0167d97ae069d580616dffead00f1a76ad2621e
jinyuo <aleydis95@gmail.com>
2024-01-01 19:48:48 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.18 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,16 @@
+import re
+
+def solution(files):
+    answer = []
+
+    for f in files:
+        split = re.match('(?P<head>\D+)(?P<number>\d+)(?P<tail>.*)', f).groupdict()
+        split['file'] = f
+        split['head'] = split['head'].lower()
+        split['number'] = int(split['number'])
+        answer.append(split)
+
+    answer = sorted(answer, key=lambda x: (x['head'], x['number']))
+    answer = [a['file'] for a in answer]
+
+    return answer
