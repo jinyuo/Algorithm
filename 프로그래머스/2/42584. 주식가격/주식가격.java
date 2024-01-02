11d664d542d48a4dcbb3210d9ba86ab1875bafc2
jinyuo <aleydis95@gmail.com>
2024-01-02 21:40:23 +0900

    [level 2] Title: 주식가격, Time: 11.88 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -7,14 +7,12 @@ class Solution {
         for(int i = 0; i < prices.length; i++){
             for(int j = i + 1; j < prices.length; j++){
                 if(prices[i] > prices[j] || j == prices.length - 1){
-                    //System.out.printf("price[%d] = %d, price[%d] = %d\n", i, prices[i], j, prices[j]);
                     answer[i] = j - i;
                     break;
                 }
                 
             }
         }
-        //System.out.println(Arrays.toString(answer));
         return answer;
     }
 }
