17765981ce0dd72a9d51dd7d2fd2abe499ef138c
jinyuo <aleydis95@gmail.com>
2024-03-07 15:38:22 +0900

    [Silver I] Title: 오르막 수, Time: 48 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,9 @@
+import sys
+
+memo = [[1 for i in range(10)]]
+divided = 10007
+
+n = int(sys.stdin.readline())
+for i in range(1, n):
+    memo.append([sum(memo[-1][i:]) % divided for i in range(10)])
+print(sum(memo[-1]) % divided)
