abb54bbed9b23acdc8f03e24f503fea0c8785ac1
jinyuo <aleydis95@gmail.com>
2024-03-13 00:08:17 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,15 +1,39 @@
 import re
 
-def solution(new_id):   
-    new_id = new_id.lower()
-    new_id = re.sub('[^a-z0-9\-_\.]', '', new_id)
-    new_id = re.sub('\.+', '.', new_id)
-    new_id = re.sub('^\.|\.$', '', new_id)
-    
-    new_id = new_id[:15] if new_id else 'a'
-    new_id = re.sub('\.$', '', new_id)
+def solution(new_id):
+    allow_str = '[a-z]|[0-9]|-|_|\.'
+    max_len = 16
+    min_len = 3
 
-    if len(new_id) < 3:
-        new_id += new_id[-1] * (3 - len(new_id))
-    
+    # step - 1
+    new_id = new_id.lower()
+
+    # step - 2
+    new_id = ''.join(re.findall(allow_str, new_id))
+
+    # step - 3
+    while '..' in new_id:
+        new_id = new_id.replace('..', '.')
+
+    # step - 4
+    while re.search('^\.', new_id):
+        new_id = new_id[1:]
+    while re.search('\.$', new_id):
+        new_id = new_id[:-1]    
+
+    # step - 5
+    if not new_id:
+        new_id = 'a'
+
+    # step - 6
+    if len(new_id) >= max_len:
+        new_id = new_id[:max_len - 1]
+    while re.search('\.$', new_id):
+        new_id = new_id[:-1]    
+
+    # step - 7
+    if len(new_id) < min_len:
+        new_id += new_id[-1] * min_len
+        new_id = new_id[:min_len]
+
     return new_id
