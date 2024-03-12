99b8ed21d536a5f229cbca813908238867afa055
jinyuo <aleydis95@gmail.com>
2024-03-12 21:21:51 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,2 +1,2 @@
-a, b, c = sorted(map(int, input().split()))
-print([c, b + 10, b * 10 + 100][[a, b, c].count(b) - 1] * 100)
+a,b,c=sorted(map(int,input().split()))
+print([c,b+10,b*10+100][[a,b,c].count(b)-1]*100)
