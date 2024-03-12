75c275cb8ec17e91417ff5429cc65dd27f347f5b
jinyuo <aleydis95@gmail.com>
2024-03-12 21:02:06 +0900

    [Silver IV] Title: 차집합, Time: 608 ms, Memory: 261656 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin.readline()
 a = list(map(int, sys.stdin.readline().split()))
 b = set(map(int, sys.stdin.readline().split()))
-sub = sorted([i for i in a if i not in b])
+sub = [i for i in sorted(a) if i not in b]
 if len(sub) == 0:
     print(0)
 else:
