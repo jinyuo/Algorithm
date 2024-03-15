0975cd609224e5a7eb3fb368e8d8ba3dd8e19d73
jinyuo <aleydis95@gmail.com>
2024-03-15 14:40:02 +0900

    [Silver IV] Title: 수 찾기, Time: 180 ms, Memory: 51872 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -4,7 +4,6 @@ N = int(sys.stdin.readline())
 A = set(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
 nums = list(map(int, sys.stdin.readline().split()))
-set_num = set(nums)
-intersection = A & set_num
+intersection = A & set(nums)
 for n in nums:
     print(1 if n in intersection else 0)
