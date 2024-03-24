dc3ed4abf9c2fa71daffde5130ad05103e21be88
jinyuo <aleydis95@gmail.com>
2024-03-24 18:52:47 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9168 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -4,6 +4,7 @@ N = int(sys.stdin.readline())
 list_nums = [0 for _ in range(10000)]
 for _ in range(N):
     list_nums[int(sys.stdin.readline()) - 1] += 1
-for i, cnt in enumerate(list_nums):
-    for _ in range(list_nums[i]):
-        print(i + 1)
+list_nums = [(i + 1, cnt) for i, cnt in enumerate(list_nums) if cnt > 0]
+for i, cnt in list_nums:
+    for _ in range(cnt):
+        print(i)
