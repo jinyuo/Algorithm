3755819ceda5c5e1100ce5c2d2ac6e834c9802ee
jinyuo <aleydis95@gmail.com>
2024-03-24 13:52:43 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 34768 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -4,6 +4,9 @@ from string import ascii_lowercase
 r = 31
 M = 1234567891
 L = int(sys.stdin.readline())
-list_a = [ascii_lowercase.index(s) + 1 for s in sys.stdin.readline().strip()]
-list_a = [a * (r ** i) % M for i, a in enumerate(list_a)]
-print(sum(list_a))
+list_a = sys.stdin.readline().strip()
+
+H = 0
+for i, a in enumerate(list_a):
+    H += (ascii_lowercase.index(a) + 1) * (r ** i) % M
+print(H % M)
