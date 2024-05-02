020fc25afef2651d696783792c9d07bf4acfc395
jinyuo <aleydis95@gmail.com>
2024-05-02 23:38:02 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 4.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 3.67 ms
+메모리: 10.4 MB, 시간: 4.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 02일 22:56:44
+2024년 05월 02일 23:38:01
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,25 +1,31 @@
-def make_gift_table(friends, gifts):
+def make_gifts_table(friends, gifts):
     num_friends = len(friends)
+    # 선물 지수 값을 위한 공간 포함하여 테이블 초기화
     table_gifts = [[0 for _ in range(num_friends + 1)] for _ in range(num_friends)]
     for gift in gifts:
         sender, receiver = gift.split()
         table_gifts[friends.index(sender)][friends.index(receiver)] += 1
     
+    # 선물 지수 계산
     for i, data in enumerate(table_gifts):    
-        table_gifts[i][num_friends] = sum(data) - sum([g[i] for g in table_gifts])
+        table_gifts[i][num_friends] = sum(data) - sum([row[i] for row in table_gifts])
     return table_gifts
 
 def solution(friends, gifts):
     answer = 0
     num_friends = len(friends)
-    table_gifts = make_gift_table(friends, gifts)
+    table_gifts = make_gifts_table(friends, gifts)
     
+    # 다음 달에 받을 선물의 수
     list_gifts = [0 for _ in range(num_friends)]    
     for i in range(num_friends):
+        # 순회 범위 조정
         for j in range(i + 1, num_friends):
+            # 선물을 주고받은 기록이 있는 경우
             if (table_gifts[i][j] != 0 or table_gifts[j][i] != 0) and (table_gifts[i][j] != table_gifts[j][i]):
                 list_gifts[i if table_gifts[i][j] > table_gifts[j][i] else j] += 1
             else:
+                # 선물 지수 값 비교
                 if table_gifts[i][num_friends] > table_gifts[j][num_friends]:
                     list_gifts[i] += 1
                 elif table_gifts[i][num_friends] < table_gifts[j][num_friends]:
