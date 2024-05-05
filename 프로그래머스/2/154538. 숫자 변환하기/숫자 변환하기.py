5ae7416676fb603b61839b8c2c885464ac0eef83
jinyuo <aleydis95@gmail.com>
2024-05-06 01:18:23 +0900

    [level 2] Title: 숫자 변환하기, Time: 264.60 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,14 +1,13 @@
 def solution(x, y, n):
-    memo = [(False, 0) for _ in range(x + 1)]
-    memo[x] = (True, 0)
+    memo = [-1 for _ in range(x + 1)]
+    memo[x] = 0
     for i in range(x + 1, y + 1):
-        cnt = 0
-        if i - n >= x and memo[i - n][0]:
-            cnt = memo[i - n][1] + 1
-        if i % 2 == 0 and memo[i // 2][0]:
-            cnt = min(memo[i // 2][1] + 1, cnt) if cnt > 0 else memo[i // 2][1] + 1
-        if i % 3 == 0 and memo[i // 3][0]:
-            cnt = min(memo[i // 3][1] + 1, cnt) if cnt > 0 else memo[i // 3][1] + 1
-        memo.append((cnt != 0, cnt))
-
-    return memo[-1][1] if memo[-1][0] else -1
+        cnt = -1
+        if i - n >= x and memo[i - n] >= 0:
+            cnt = memo[i - n] + 1
+        if i % 2 == 0 and memo[i // 2] >= 0:
+            cnt = min(memo[i // 2] + 1, cnt) if cnt >= 0 else memo[i // 2] + 1
+        if i % 3 == 0 and memo[i // 3] >= 0:
+            cnt = min(memo[i // 3] + 1, cnt) if cnt >= 0 else memo[i // 3] + 1
+        memo.append(cnt)
+    return memo[-1] if memo[-1] >= 0 else -1
