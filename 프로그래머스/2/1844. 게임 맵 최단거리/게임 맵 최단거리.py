0e4a043a1a3e17f050772111e35320eb26de6462
jinyuo <aleydis95@gmail.com>
2024-05-10 15:53:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.78 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -4,21 +4,17 @@ def solution(maps):
     visited = [[0 for _ in range(n)] for _ in range(m)]
     queue = [(0, 0)]
     visited[0][0] = 1
+    nxt_gap = [(1, 0), (0, 1), (-1, 0), (0, -1)]
     while queue:
         i, j = queue.pop(0)
+        
+        for x, y in nxt_gap:
+            nx, my = i + x, j + y
+            
+            if 0 <= nx < m and 0 <= my < n and maps[nx][my] == 1 and visited[nx][my] == 0:
+                queue.append((nx, my))
+                visited[nx][my] = visited[i][j] + 1
 
-        if i + 1 < m and maps[i + 1][j] == 1 and visited[i + 1][j] == 0:
-            queue.append((i + 1, j))
-            visited[i + 1][j] = visited[i][j] + 1
-        if j + 1 < n and maps[i][j + 1] == 1 and visited[i][j + 1] == 0:
-            queue.append((i, j + 1))
-            visited[i][j + 1] = visited[i][j] + 1
-        if i - 1 >= 0 and maps[i - 1][j] == 1 and visited[i - 1][j] == 0:
-            queue.append((i - 1, j))
-            visited[i - 1][j] = visited[i][j] + 1
-        if j - 1 >= 0 and maps[i][j - 1] == 1 and visited[i][j - 1] == 0:
-            queue.append((i, j - 1))
-            visited[i][j - 1] = visited[i][j] + 1
             
     if visited[-1][-1] == 0:
         return -1
