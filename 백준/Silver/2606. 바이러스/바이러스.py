0b4f06bb25449ec1e67c86475c084dbd1b29d3bd
jinyuo <aleydis95@gmail.com>
2024-05-12 15:20:29 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,24 +1,18 @@
 import sys
 
 
-def make_graph(cnt_pair: int) -> dict:
-    graph = {}
+def make_graph(cnt_computor: int, cnt_pair: int) -> dict:
+    graph = {k: [] for k in range(1, cnt_computor + 1)}
     for _ in range(cnt_pair):
         pair = list(map(int, sys.stdin.readline().split()))
         len_pair = len(pair)
         for i in range(len_pair):
-            if pair[i] in graph:
-                graph[pair[i]].append(pair[len_pair - 1 - i])
-            else:
-                graph[pair[i]] = [pair[len_pair - 1 - i]]
+            graph[pair[i]].append(pair[len_pair - 1 - i])
     return graph
 
 
 def bfs(graph: dict, start: int = 1) -> dict:
     visited = {k: False for k in graph.keys()}
-
-    if not start in graph.keys():
-        return visited
 
     queue = [start]
     visited[start] = True
@@ -34,6 +28,6 @@ def bfs(graph: dict, start: int = 1) -> dict:
 cnt_computor = int(sys.stdin.readline())
 cnt_pair = int(sys.stdin.readline())
 start = 1
-graph = make_graph(cnt_pair)
+graph = make_graph(cnt_computor, cnt_pair)
 infection = bfs(graph)
 print(len([k for k, v in infection.items() if k != start and v]))
