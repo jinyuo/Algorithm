7c42e90cbfbecc555176857a77862ce74d9bcb7b
jinyuo <aleydis95@gmail.com>
2024-05-12 15:23:26 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,19 +1,16 @@
 import sys
 
-
 def make_graph(cnt_computor: int, cnt_pair: int) -> dict:
     graph = {k: [] for k in range(1, cnt_computor + 1)}
     for _ in range(cnt_pair):
-        pair = list(map(int, sys.stdin.readline().split()))
-        len_pair = len(pair)
-        for i in range(len_pair):
-            graph[pair[i]].append(pair[len_pair - 1 - i])
+        a, b = map(int, sys.stdin.readline().split())
+        graph[a].append(b)
+        graph[b].append(a)
     return graph
 
 
 def bfs(graph: dict, start: int = 1) -> dict:
     visited = {k: False for k in graph.keys()}
-
     queue = [start]
     visited[start] = True
     while queue:
@@ -23,7 +20,6 @@ def bfs(graph: dict, start: int = 1) -> dict:
                 queue.append(i)
                 visited[i] = True
     return visited
-
 
 cnt_computor = int(sys.stdin.readline())
 cnt_pair = int(sys.stdin.readline())
