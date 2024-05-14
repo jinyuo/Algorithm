aa718ffdd6ec1a53cbefae68da974b03e24af594
jinyuo <aleydis95@gmail.com>
2024-05-14 21:10:47 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -8,22 +8,20 @@ def make_graph(cnt_computor: int, cnt_pair: int) -> dict:
         graph[b].append(a)
     return graph
 
+def dfs(graph: dict, start: int = 1) -> dict:
+    visited = []
+    need_visit = [start]
 
-def bfs(graph: dict, start: int = 1) -> dict:
-    visited = {k: False for k in graph.keys()}
-    queue = [start]
-    visited[start] = True
-    while queue:
-        curr = queue.pop(0)
-        for i in graph[curr]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
+    while need_visit:
+        curr = need_visit.pop()
+        if curr not in visited:
+            visited.append(curr)
+            need_visit.extend(graph[curr])
     return visited
 
 cnt_computor = int(sys.stdin.readline())
 cnt_pair = int(sys.stdin.readline())
 start = 1
 graph = make_graph(cnt_computor, cnt_pair)
-infection = bfs(graph)
-print(len([k for k, v in infection.items() if k != start and v]))
+infection = dfs(graph, start)
+print(len([i for i in infection if i != start]))
