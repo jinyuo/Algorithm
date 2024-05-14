104faeada8cd3705680456a4f614b55562147203
jinyuo <aleydis95@gmail.com>
2024-05-14 22:51:06 +0900

    [Silver I] Title: 단지번호붙이기, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -5,32 +5,29 @@ def bfs(maps, n, m, start=(0, 0)):
     visited = [[False for _ in range(m)] for _ in range(n)]
     directions = [(1, 0), (0, -1), (-1, 0), (0, 1)]
     queue = [start]
-    visited[start[0]][start[1]] = 1
+    visited[start[0]][start[1]] = True
 
     while queue:
         i, j = queue.pop(0)
-
         for x, y in directions:
             ni, ny = i + x, j + y
-
             if 0 <= ni < n and 0 <= ny < m and maps[ni][ny] == 1:
                 if visited[ni][ny] == 0:
                     queue.append((ni, ny))
                     visited[ni][ny] = True
-
     return visited
 
 
 n = int(sys.stdin.readline())
 maps = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
 
-visited = [[0 for _ in range(n)] for _ in range(n)]
+visited = [[False for _ in range(n)] for _ in range(n)]
 list_cnt = []
 for i in range(n):
     for j in range(n):
-        if maps[i][j] == 1 and visited[i][j] == 0:
+        if maps[i][j] == 1 and not visited[i][j]:
             temp = bfs(maps, n, n, start=(i, j))
-            visited = [[temp[x][y] + visited[x][y] for y in range(n)] for x in range(n)]
+            visited = [[temp[x][y] | visited[x][y] for y in range(n)] for x in range(n)]
             list_cnt.append(len([i for i in sum(temp, []) if i]))
 
 list_cnt.sort()
