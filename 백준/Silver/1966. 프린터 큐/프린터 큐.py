42567e11312ec0575bb80a13b532093897480efc
jinyuo <aleydis95@gmail.com>
2024-05-25 22:00:05 +0900

    [Silver III] Title: 프린터 큐, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,19 @@
+import sys
+
+case = int(sys.stdin.readline())
+for _ in range(case):
+    N, M = map(int, sys.stdin.readline().split())
+    papers = list(map(int, sys.stdin.readline().split()))
+    queue = list(enumerate(papers))
+
+    answer = 1
+    while queue:
+        v = queue.pop(0)
+        if queue and v[1] < max(q[1] for q in queue):
+            queue.append(v)
+        else:
+            if M == v[0]:
+                break
+            else:
+                answer += 1
+    print(answer)
