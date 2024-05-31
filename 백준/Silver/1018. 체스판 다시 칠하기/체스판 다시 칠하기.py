e93ce27302ed12cd6b6662323382c9e1055b7134
jinyuo <aleydis95@gmail.com>
2024-06-01 00:00:32 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,14 +1,15 @@
 import sys
 
-w_board = ['WB' * 4 if i % 2 == 0 else 'BW' * 4 for i in range(8)]
-b_board = ['BW' * 4 if i % 2 == 0 else 'WB' * 4 for i in range(8)]
+size = 8
+w_board = ['WB' * (size // 2) if i % 2 == 0 else 'BW' * (size // 2) for i in range(size)]
+b_board = ['BW' * (size // 2) if i % 2 == 0 else 'WB' * (size // 2) for i in range(size)]
 
 
 def check_board(board, i, j, mode='W'):
     cnt = 0
     using_board = w_board if mode == 'W' else b_board
-    for x in range(8):
-        for y in range(8):
+    for x in range(size):
+        for y in range(size):
             if using_board[x][y] != board[i + x][j + y]:
                 cnt += 1
     return cnt
@@ -16,6 +17,6 @@ def check_board(board, i, j, mode='W'):
 
 N, M = map(int, sys.stdin.readline().split())
 board = [sys.stdin.readline().strip() for _ in range(N)]
-result = [check_board(board, i, j, mode='W') for i in range(N - 7) for j in range(M - 7)]
-result.extend([check_board(board, i, j, mode='B') for i in range(N - 7) for j in range(M - 7)])
+result = [check_board(board, i, j, mode='W') for i in range(N - size + 1) for j in range(M - size + 1)]
+result.extend([check_board(board, i, j, mode='B') for i in range(N - size + 1) for j in range(M - size + 1)])
 print(min(result))
