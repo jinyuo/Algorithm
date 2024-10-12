04a24241914dd42dce8420a0388a7e9ed64d1627
jinyuo <aleydis95@gmail.com>
2024-10-12 20:23:34 +0900

    [Silver III] Title: 합 구하기, Time: 2784 ms, Memory: 126772 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.py
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -1,9 +1,9 @@
+from itertools import accumulate
+
 N = int(input().strip())
 A = list(map(int, input().split()))
 M = int(input().strip())
-list_sum = [0 for _ in range(N + 1)]
-for i in range(1, N + 1):
-    list_sum[i] = list_sum[i - 1] + A[i - 1]
+list_sum = list(accumulate(A, initial=0))
 
 for _ in range(M):
     i, j = map(int, input().split())
