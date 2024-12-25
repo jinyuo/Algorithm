ff1a4d7547ea5ad0ec19487e0d27034f1cd33d9f
jinyuo <aleydis95@gmail.com>
2024-12-25 23:14:59 +0900

    [Bronze II] Title: 저금, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/4998. 저금/저금.py
+++ 백준/Bronze/4998. 저금/저금.py
@@ -1,11 +1,10 @@
-import math
 import sys
-from math import log
+import math
 
 while True:
     try:
         N, B, M = map(float, sys.stdin.readline().strip().split(' '))
-        print(math.ceil(log(M / N, (1 + B / 100))))
+        print(math.ceil(math.log(M / N, (1 + B / 100))))
     except:
         break
    
